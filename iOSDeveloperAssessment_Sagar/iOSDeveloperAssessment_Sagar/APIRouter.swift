529a3c52aeb2801eab04985b445bdb077fcf5247
Sagar Prajapati <psagc@allstate.com>
2022-01-13 13:03:06 +0530

    initial comment

---
+++ iOSDeveloperAssessment_Sagar/iOSDeveloperAssessment_Sagar/APIRouter.swift
@@ -0,0 +1,9 @@
+//
+//  APIRouter.swift
+//  iOSDeveloperAssessment_Sagar
+//
+//  Created by psagc on 13/01/22.
+//
+
+import Foundation
+
---
+++ iOSDeveloperAssessment_Sagar/iOSDeveloperAssessment_Sagar/WebService.swift
@@ -0,0 +1,8 @@
+//
+//  WebService.swift
+//  iOSDeveloperAssessment_Sagar
+//
+//  Created by psagc on 13/01/22.
+//
+
+import Foundation
